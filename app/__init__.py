c9c24edfa73f7a89428da59cd9d659fc480cf5bf
Jaider Aprilla <jaiderreyes273@gmail.com>
2025-09-19 13:32:15 -0500

    se importó CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS # Importar CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+CORS(app)  # Habilitar CORS para la aplicación
 
 # Importar y registrar el blueprint de las rutas
 from .routes.main import main
